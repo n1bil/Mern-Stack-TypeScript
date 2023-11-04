d133f39fbb47e7dcb6a16e02c171f08155607eac
n1bil <nabil.nadzor@gmail.com>
2023-11-04 11:39:08 +0100

    express http error handling

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
     try {
@@ -15,18 +17,36 @@ export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
 
     try {
-        const node = await NoteModel.findById(noteId).exec();
-        res.status(200).json(node);
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        res.status(200).json(note);
     } catch (error) {
         next(error);
     }
+};
+
+interface CreateNoteBody {
+    title: string,
+    text?: string,
 }
 
-export const createNote: RequestHandler = async (req, res, next) => {
+export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
 
     try {
+        if (!title) {
+            throw createHttpError(400, "Note must have a title");
+        }
+
         const newNote = await NoteModel.create({
             title: title,
             text: text,
