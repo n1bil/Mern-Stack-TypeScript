cfc7adaccd583cae8e671fe854a2ce4d1c24f470
n1bil <nabil.nadzor@gmail.com>
2023-11-04 13:22:05 +0100

    update and delete note

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
-import createHttpError from "http-errors";
+import createHttpError from "http-errors";                              // библиотека предоставляет простой способ создания HTTP-ошибок с указанием кодов состояния и пользовательских сообщений об ошибках.
 import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
@@ -17,13 +17,13 @@ export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
 
     try {
-        if (!mongoose.isValidObjectId(noteId)) {
+        if (!mongoose.isValidObjectId(noteId)) {                        // является ли noteId допустимым идентификатором объекта MongoDB(например больше или меньше обычного id)
             throw createHttpError(400, "Invalid note id");
         }
 
         const note = await NoteModel.findById(noteId).exec();
 
-        if (!note) {
+        if (!note) {                                                    // если заметка не была найдена(то есть note равно null)
             throw createHttpError(404, "Note not found");
         }
 
@@ -38,6 +38,7 @@ interface CreateNoteBody {
     text?: string,
 }
 
+// first param - req; second param - req; third param - req.body; fourth param - next;
 export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
@@ -56,4 +57,67 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
     } catch (error) {
         next(error);
     }
+};
+
+interface UpdateNoteParams {
+    noteId: string,
 }
+
+interface UpdateNoteBody {
+    title?: string,
+    text?: string,
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async(req, res, next) => {
+    const noteId = req.params.noteId;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {                            // является ли noteId допустимым идентификатором объекта MongoDB(например больше или меньше обычного id)
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        if (!newTitle) {
+            throw createHttpError(400, "Note must have a title");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {                                                        // если заметка не была найдена(то есть note равно null)
+            throw createHttpError(404, "Note not found");
+        }
+
+        note.title = newTitle;
+        note.text = newText;
+
+        const updatedNote = await note.save();
+
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteNote: RequestHandler = async(req, res, next) => {
+    const noteId = req.params.noteId;
+
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {                            // является ли noteId допустимым идентификатором объекта MongoDB(например больше или меньше обычного id)
+            throw createHttpError(400, "Invalid note id");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, "Note not found");
+        }
+
+        await note.deleteOne();
+
+        res.sendStatus(204);
+
+    } catch (error) {
+        next(error);
+    }
+};
