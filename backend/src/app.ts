435b9dd8bc9c30650c0684cee213a0bac1c35b78
n1bil <nabil.nadzor@gmail.com>
2023-11-10 19:57:16 +0100

    express security

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
+import usersRoutes from "./routes/users";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
 import cors from "cors";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -14,6 +18,18 @@ app.use(cors({
 app.use(morgan("dev"));     // log information
 app.use(express.json());    // watch later
 
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION_STRING
+    }),
+}));
+
+app.use("/api/users", usersRoutes);
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
